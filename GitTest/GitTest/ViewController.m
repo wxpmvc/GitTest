bff1d638aa5f4c426222debc43df16bf77ed1cd4
wxpmvc <304822897@qq.com>
2017-10-13 14:43:47 +0800

    fifth commit

--- GitTest/GitTest/ViewController.m
+++ GitTest/GitTest/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "Dog.h"
+
 
 @interface ViewController ()
 
@@ -16,7 +18,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    Dog *dog = [[Dog alloc]init];
+    [dog bark];
+    [dog run];
+    
+
 }
 
 

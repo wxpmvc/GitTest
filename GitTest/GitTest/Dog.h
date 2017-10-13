b068869f3ffd9383b4ec04ecb744fb270384cb71
wxpmvc <304822897@qq.com>
2017-10-13 14:36:31 +0800

    third commit

---
+++ GitTest/GitTest/Dog.h
@@ -0,0 +1,18 @@
+//
+//  Dog.h
+//  GitTest
+//
+//  Created by wxp on 2017/10/13.
+//  Copyright © 2017年 wxp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@property (nonatomic, assign) float age;
+
+- (void)bark;
+
+
+@end
---
+++ GitTest/GitTest/Dog.m
@@ -0,0 +1,19 @@
+//
+//  Dog.m
+//  GitTest
+//
+//  Created by wxp on 2017/10/13.
+//  Copyright © 2017年 wxp. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+- (void)bark {
+    NSLog(@"wang wang wang");
+
+}
+
+
+@end
